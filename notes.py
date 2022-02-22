8e62f0bc0c04b08b6ed2e2277ba6eafb1d4acac3
Ramon Asuncion <asuncionbatista@gmail.com>
2022-02-21 23:29:17 -0500

    Remove unused/commented code.

--- notes.py
+++ notes.py
@@ -1,12 +1,12 @@
+from email.mime import image
 import os
 import tkinter as tk
 from tkinter import messagebox
 class Notes:
-    def __init__(self, master=None):
+    def __init__( self, master=None ):
         self.master = master
         
-        
-    def register_user(self):
+    def register_user( self ):
         username_info = self.username.get()
         password_info = self.password.get()
         
@@ -20,27 +20,13 @@ class Notes:
             file.write("Password:\n")
             file.write(password_info)
             file.close()
-            
-            # tk.Label(self.register_screen, text="Registration successful!", fg="#36BB00", font=("Lato", 12)).pack()
-            # self.register_screen.config(text='Registration successful!')
-            # self.register_screen.after(2000, lambda:self.register_screen.config(text=''))
-            
-            # login_tk.Button_check_test = tk.Label(self.register_screen, text="Registration successful!")
-            # login_tk.Button_check_test.pack(fg="#36BB00", font=("Lato", 12))
-            #self.register_screen.after(2000, self.register_screen.destroy)
-            
         except FileExistsError:
-            # tk.Label(self.register_screen, text="User Already Created", fg="#F18300", font=("Lato", 12)).pack()
             self.register_screen.config(text='User Already Created!')
-            # self.register_screen.after(2000, lambda:self.register_screen.config(text=''))
-            # TODO: Make this exception unnecessary maybe by creating the file instead of trying to chdir when its not found? 
-        except FileNotFoundError:
-            pass
             
-        self.entry_username.delete(0,END)
-        self.entry_password.delete(0,END)
+        self.entry_username.delete(0, tk.END)
+        self.entry_password.delete(0, tk.END)
 
-    def save_file(self):
+    def save_file( self ):
         create_notes = self.file_name.get()
         get_notes = create_notes.get()
         
@@ -50,7 +36,7 @@ class Notes:
         
         tk.Label(self.notes_screen, text="Text File Saved", fg="#36BB00", font=("Lato", 12)).pack()
 
-    def login_completed(self):
+    def login_completed( self ):
         self.dashboard_screen = tk.Toplevel(self.master)
         self.dashboard_screen.title("Dashboard")
         self.dashboard_screen.geometry("350x250")
@@ -62,17 +48,8 @@ class Notes:
         tk.Button(self.dashboard_screen, text="View secret note", command=self.view_notes).pack()
         tk.Button(self.dashboard_screen, text="Delete secret note", command=self.delete_notes).pack()
         
-        self.dashboard_screen.protocol("WM_DELETE_WINDOW", self.close_gui_windows)
 
-    # def close_gui_windows(self):
-    #     if messagebox.askokcancel("Warning", "Do you want to exit dashboard?"):
-            # TODO: When person signout of their account it needs to exit out of the folder (testing --> not the most thought out way)
-            
-    #         # self.dashboard_screen.destory()
-            # TODO: Go back to the main directory folder. ("..") can be risky due to it being access to full location where the folder is located.
-    #         os.chdir("./test/")   
-
-    def delete_notes(self):
+    def delete_notes( self ):
         self.register_screen0 = tk.Toplevel(self.master)
         self.register_screen0.title("Delete")
         self.register_screen0.geometry("350x250")
@@ -88,7 +65,7 @@ class Notes:
         tk.Button(self.register_screen0, command=self.delete_notes_message, text="OK").pack()
 
 
-    def delete_notes_message(self):
+    def delete_notes_message( self ):
         delete = self.delete_file.get()
         os.remove(delete)
         
@@ -99,7 +76,7 @@ class Notes:
         tk.Label(self.register_screen1, text=delete+" has been removed").pack()
 
 
-    def create_secret_notes(self):        
+    def create_secret_notes( self ):        
         self.file_name = tk.StringVar()
         create_notes = tk.StringVar()
         
@@ -113,7 +90,7 @@ class Notes:
         tk.Entry(self.notes_screen, textvariable=create_notes).pack()
         tk.Button(self.notes_screen, text="Save", command=self.save_file).pack()
 
-    def choose_files(self):
+    def choose_files( self ):
         open_file_screen = tk.Toplevel(self.master)
         open_file_screen.title("Info")
         open_file_screen.geometry("350x250")
@@ -128,7 +105,7 @@ class Notes:
         tk.Entry(open_file_screen, textvariable=self.file_name).pack()
         tk.Button(open_file_screen, command=self.view_notes, text="OK").pack()
         
-    def view_notes(self):    
+    def view_notes( self ):    
         read_file_screen = tk.Toplevel(self.master)
         read_file_screen.title("Notes")
         read_file_screen.geometry("350x250")    
@@ -138,17 +115,20 @@ class Notes:
         
         tk.Label(read_file_screen, text=data).pack()
 
-    def login_verify(self):
+    def login_verify( self ):
         get_username = self.user_verify.get()
         get_password = self.pass_verify.get()
 
         list_of_files = os.listdir("./test/")
         if get_username in list_of_files:
             self.file_name = str(get_username)
+            
             # TODO: Remove after debugging is finished. 
             print("System: Debug file name " + self.file_name)
+            
             # TODO: Files not saving in test.
             os.chdir("./test/" + self.file_name)
+            
             file_open = open(get_username, "r")
             verify = file_open.read().splitlines()
             if get_password in verify:
@@ -159,7 +139,7 @@ class Notes:
             tk.Label(self.login_screen, text="User not found!", fg="#E81500", font=("Lato", 12)).pack()
 
 
-    def login_page(self):
+    def login_page( self ):
         self.login_screen = tk.Toplevel(self.master)
         self.login_screen.title("Login")
         self.login_screen.geometry("350x250")
@@ -182,14 +162,17 @@ class Notes:
         tk.Label(self.login_screen, text="").pack()
         self.entry_password1.config(show="*")
         
+        eye_icon_on = tk.PhotoImage(file="./assets/images/view.png")
+        eye_icon_off = tk.PhotoImage(file="./assets/images/invisible.png")
+        
         # TODO: Change up the tk.Buttons to have eyes instead of a bar. 
-        tk.Button(self.login_screen, text="Show password!", height="1", width="10", command=lambda: self.password_show("show_login_password")).pack()
-        tk.Button(self.login_screen, text="Hide password!", height="1", width="10", command=lambda: self.password_show("hide_login_password")).pack()
+        tk.Button(self.login_screen, image=eye_icon_on, height="25", width="25", command=lambda: self.password_show("show_login_password")).pack()
+        tk.Button(self.login_screen, image=eye_icon_off, height="1", width="10", command=lambda: self.password_show("hide_login_password")).pack()
+        
         tk.Button(self.login_screen, text="Login", height="1", width="10", command=self.login_verify).pack()
-                
 
-
-    def password_show(self, button_click):
+    def password_show( self, button_click ):
+        print("System: Debug button click " + button_click)
         if button_click == "show_login_password":
             self.entry_password1.config(show="") 
         elif button_click == "hide_login_password":
@@ -222,12 +205,13 @@ class Notes:
         tk.Label(self.register_screen, text="").pack() 
         self.entry_password.config(show="*")
         
+        # TODO: Change up the tk.Buttons to have eyes instead of a bar. 
         tk.Button(self.register_screen, text="Show password!", height="1", width="12", command=lambda: self.password_show("show_register_password")).pack()
         tk.Button(self.register_screen, text="Hide password!", height="1", width="12", command=lambda: self.password_show("hide_register_password")).pack()
         tk.Button(self.register_screen, text="Register", height="1", width="10", command=self.register_user).pack()
 
         
-    def screen(self):        
+    def screen( self ):        
         self.master.geometry("350x250")
         self.master.title("Secret Notes")
         self.master.iconphoto(True, tk.PhotoImage(file="./assets/images/favicon.png"))
